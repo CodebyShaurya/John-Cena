2c697a8f3d50530889f0f4cba09830c61d647564
Shaurya Gupta <ShauryaGupta118@gmail.com>
2025-05-14 18:59:32 +0530

    Refactor CountdownSection component for improved readability and layout consistency

--- src/components/CountdownSection.tsx
+++ src/components/CountdownSection.tsx
@@ -24,16 +24,16 @@ const CountdownSection: React.FC = () => {
         {/* Center Content */}
         <div className={`h-screen  w-1/3 md:w-1/2 text-center z-10 ${isVisible ? 'fade-in' : 'opacity-0'}`}>
           {/* Hustle Loyalty Respect Logo */}
-           <div className="logo-pulse">
-     <img
-        src="https://raw.githubusercontent.com/CodebyShauryaIPU/John-Cena/refs/heads/main/wwe.png"
-        alt="Cena Logo"
-        className="w-32 h-32 md:w-48 md:h-48 mx-auto md:mb-1 mb-10  animate-pulse-slow"
-      />
-    </div>
+          <div className="logo-pulse">
+            <img
+              src="https://raw.githubusercontent.com/CodebyShauryaIPU/John-Cena/refs/heads/main/wwe.png"
+              alt="Cena Logo"
+              className="w-32 h-32 md:w-48 md:h-48 mx-auto md:mb-1 mb-10 animate-pulse-slow"
+            />
+          </div>
 
           {/* Main Countdown */}
-          <div className="spotlight relative max-w-4xl mx-auto  md:p-8 md:p-16">
+          <div className="spotlight relative max-w-4xl mx-auto md:p-8 md:p-16">
             <CountdownTimer />
           </div>
 
@@ -44,6 +44,10 @@ const CountdownSection: React.FC = () => {
               YOU CAN'T SEE ME
             </h2> */}
           </div>
+
+          <div className="absolute bottom-4 w-full text-center text-white text-sm">
+        Developed by - <a href="https://github.com/CodebyShaurya/" target="_blank" rel="noopener noreferrer" className="underline">Shaurya</a>
+      </div>
         </div>
 
         {/* Right Slideshow */}
@@ -51,6 +55,9 @@ const CountdownSection: React.FC = () => {
           <Slideshow direction="right" />
         </div>
       </div>
+
+      {/* Developed By Section */}
+      
     </section>
   );
 };
