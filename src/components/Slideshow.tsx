60c8dcb7122524769246976d9895305b5b97ba97
Shaurya Gupta <ShauryaGupta118@gmail.com>
2025-06-07 13:39:48 +0530

    Update image source in Slideshow component to a new URL

--- src/components/Slideshow.tsx
+++ src/components/Slideshow.tsx
@@ -8,7 +8,7 @@ const Slideshow: React.FC<SlideshowProps> = ({ direction }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const images = [
-    "https://scontent.fdel8-1.fna.fbcdn.net/v/t39.30808-6/491865580_1103079511839004_1216561748529453558_n.jpg?stp=dst-jpg_s640x640_tt6&_nc_cat=103&ccb=1-7&_nc_sid=127cfc&_nc_ohc=mMMLi3COVk4Q7kNvwE3HMqD&_nc_oc=AdlXuC92SWjl7dFn6PYhW3gdf-rKgW3nEyNiFsutNwyvs_m__RbiDRg7RCv-GN-pdcc&_nc_zt=23&_nc_ht=scontent.fdel8-1.fna&_nc_gid=Goaq12soMMbWhxvNRkxHAQ&oh=00_AfIVOOQw9JnF5GDhw6F_YFlcTnccca3EUt_qakFhEr0PbA&oe=6826ED87",
+    "https://www.kupywrestlingwallpapers.info/wp-content/uploads/2025/03/john-cena-heel-turn-wallpaper-preview.jpg",
     "https://cdn.vox-cdn.com/thumbor/4Ll2nnQsZ9QgdWZ0z2VRFxqQs3I=/0x0:1772x947/1200x800/filters:focal(737x494:1019x776)/cdn.vox-cdn.com/uploads/chorus_image/image/73518861/Screenshot_2024_08_13_at_12.48.51_PM.0.png",
     "https://cdn.vox-cdn.com/thumbor/aQa041Q7PYJmdxkRcy88zPe2UIY=/170x0:1252x721/1310x873/filters:format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/47523163/RAW_1143_Photo_165-791015758.0.0.0.jpg",
     "https://i.ytimg.com/vi/4jlPO9rDjp0/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLB_RAmrz_0l9G_VvMdWx64C8Bwwpg",
