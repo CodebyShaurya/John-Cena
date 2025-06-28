2f6908bd515d51e84789f014302fd31af77aeaab
Shaurya Gupta <ShauryaGupta118@gmail.com>
2025-06-29 01:20:32 +0530

    Update countdown value in CountdownTimer component to reflect the correct number of nights left

--- src/components/CountdownTimer.tsx
+++ src/components/CountdownTimer.tsx
@@ -4,7 +4,7 @@ const CountdownTimer: React.FC = () => {
   return (
     <div className="countdown-container">
       <div className="text-8xl sm:text-9xl md:text-[7rem] lg:text-[9rem] font-bebas count-text text-white">
-        17
+        16
       </div>
       <div className="text-lg md:text-xl mt-4 font-montserrat text-gray-300">
         NIGHTS LEFT
